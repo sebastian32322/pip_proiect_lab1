01349391bb28d8dad877658ee8013e7ab0444136
Bodescu Razvan <razvan-sebastian.bodescu@student.tuiasi.ro>
2022-05-09 15:45:14 +0300

    primul commit

--- src/SalutPip/Afisare.java
+++ src/SalutPip/Afisare.java
@@ -4,6 +4,9 @@ public class Afisare {
 
 	public static void main(String[] args) {
 		System.out.println("SALUT PIP - 09.05.2022");
+		
+		int a = 2;
+		System.out.println("primul commit din proiect 15:42 ");
 
 	}
 
